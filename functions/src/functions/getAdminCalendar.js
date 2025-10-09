3b06ae3d9d946c56d271932abcf631d323fdf7ea
oyvind3 <71570586+oyvind3@users.noreply.github.com>
2025-10-09 22:13:59 +0200

    Restructure Azure Functions directories

--- functions/src/functions/getAdminCalendar.js
+++ functions/src/functions/getAdminCalendar.js
@@ -1,6 +1,6 @@
 const { app } = require('@azure/functions');
-const { createJsonResponse } = require('./shared/http');
-const { listBookings } = require('./shared/bookingStore');
+const { createJsonResponse } = require('../shared/http');
+const { listBookings } = require('../shared/bookingStore');
 
 /**
  * Admin calendar endpoint with full requester visibility.
